32b66372076f54b69e4c03dbb8edc0215158663b
Carlos Murillo <carlosdanielmurillojiron144@gmail.com>
2022-05-16 15:19:04 -0600

    added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
@@ -6,5 +8,24 @@ app.get("/", function (req, res) {
   res.send("<h1> Hello World! </h1>");
 });
 
-app.get("/")
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/index", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
 app.listen(3000);
